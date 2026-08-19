8c0e83dbc956a46d4fb4f9e188a937c03b458be4
VaheC <vcharchian@gmail.com>
2026-08-19 16:05:10 +0400

    redundant comments removed

--- project/main.py
+++ project/main.py
@@ -1,31 +1,3 @@
-# from fastapi import FastAPI
-# from contextlib import asynccontextmanager
-
-# from db.db import Base
-# from db.db_session import engine
-
-# from routers import auth, projects, documents
-
-
-# async def create_db_and_tables():
-#     async with engine.begin() as conn:
-#         await conn.run_sync(Base.metadata.create_all)
-
-
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     await create_db_and_tables()
-#     yield
-
-
-# app = FastAPI(
-#     lifespan=lifespan
-# )
-
-# app.include_router(auth.router)
-# app.include_router(projects.router)
-# app.include_router(documents.router)
-
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from fastapi.openapi.utils import get_openapi
